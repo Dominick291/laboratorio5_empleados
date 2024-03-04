0e7a1d419d73888323c6181116b0bf906ee6fd7a
dpalpirez <dpalpirez@LAB4-22>
2024-03-04 09:45:59 -0600

    Agregar archivos de proyecto.

---
+++ laboratorio5_empleados/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿
+namespace laboratorio5_empleados
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridViewEmpleados = new System.Windows.Forms.DataGridView();
+            this.dataGridViewAsistencia = new System.Windows.Forms.DataGridView();
+            this.cargar = new System.Windows.Forms.Button();
+            this.dataGridViewReporte = new System.Windows.Forms.DataGridView();
+            this.calcular = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewEmpleados)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewAsistencia)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewReporte)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridViewEmpleados
+            // 
+            this.dataGridViewEmpleados.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewEmpleados.Location = new System.Drawing.Point(36, 72);
+            this.dataGridViewEmpleados.Name = "dataGridViewEmpleados";
+            this.dataGridViewEmpleados.Size = new System.Drawing.Size(240, 150);
+            this.dataGridViewEmpleados.TabIndex = 0;
+            // 
+            // dataGridViewAsistencia
+            // 
+            this.dataGridViewAsistencia.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewAsistencia.Location = new System.Drawing.Point(324, 72);
+            this.dataGridViewAsistencia.Name = "dataGridViewAsistencia";
+            this.dataGridViewAsistencia.Size = new System.Drawing.Size(240, 150);
+            this.dataGridViewAsistencia.TabIndex = 1;
+            // 
+            // cargar
+            // 
+            this.cargar.Location = new System.Drawing.Point(247, 270);
+            this.cargar.Name = "cargar";
+            this.cargar.Size = new System.Drawing.Size(100, 55);
+            this.cargar.TabIndex = 2;
+            this.cargar.Text = "CARGAR DATOS";
+            this.cargar.UseVisualStyleBackColor = true;
+            this.cargar.Click += new System.EventHandler(this.cargar_Click);
+            // 
+            // dataGridViewReporte
+            // 
+            this.dataGridViewReporte.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewReporte.Location = new System.Drawing.Point(648, 72);
+            this.dataGridViewReporte.Name = "dataGridViewReporte";
+            this.dataGridViewReporte.Size = new System.Drawing.Size(240, 150);
+            this.dataGridViewReporte.TabIndex = 3;
+            // 
+            // calcular
+            // 
+            this.calcular.Location = new System.Drawing.Point(728, 302);
+            this.calcular.Name = "calcular";
+            this.calcular.Size = new System.Drawing.Size(75, 23);
+            this.calcular.TabIndex = 4;
+            this.calcular.Text = "Calcular";
+            this.calcular.UseVisualStyleBackColor = true;
+            this.calcular.Click += new System.EventHandler(this.calcular_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(928, 450);
+            this.Controls.Add(this.calcular);
+            this.Controls.Add(this.dataGridViewReporte);
+            this.Controls.Add(this.cargar);
+            this.Controls.Add(this.dataGridViewAsistencia);
+            this.Controls.Add(this.dataGridViewEmpleados);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewEmpleados)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewAsistencia)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewReporte)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridViewEmpleados;
+        private System.Windows.Forms.DataGridView dataGridViewAsistencia;
+        private System.Windows.Forms.Button cargar;
+        private System.Windows.Forms.DataGridView dataGridViewReporte;
+        private System.Windows.Forms.Button calcular;
+    }
+}
+
---
+++ laboratorio5_empleados/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace laboratorio5_empleados
+{
+    //cargar empleados 
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        List<Empleado> empleados = new List<Empleado>();
+        List<Asistencia> asistencias = new List<Asistencia>();
+        List<Reporte> reportes = new List<Reporte>();
+        public void CargarEmpleados()
+        {
+            
+            //leer el archivo y cargarlo de la lista 
+            String fileName = "Empleados.txt";
+
+
+            FileStream stream = new FileStream(fileName, FileMode.Open, FileAccess.Read);
+            StreamReader reader = new StreamReader(stream);
+
+            while (reader.Peek() > -1)
+            {
+                Empleado empleado = new Empleado();
+                empleado.NoEmpleado = Convert.ToInt16(reader.ReadLine());
+                empleado.Nombre = reader.ReadLine();
+                empleado.SueldoHora = Convert.ToDecimal(reader.ReadLine());
+
+                //guardar el empleado a la lista de empleados 
+                empleados.Add(empleado);
+            }
+            //Cerrar el archivo, esta linea es importante porque sino despues de correr varias veces el programa daría error de que el archivo quedó abierto muchas veces. Entonces es necesario cerrarlo despues de terminar de leerlo.
+            reader.Close();
+        }
+
+        public void CargarAsistencia()
+        {
+            string fileName = "Asistencia.txt";
+
+            FileStream stream = new FileStream(fileName, FileMode.Open, FileAccess.Read);
+            StreamReader reader = new StreamReader(stream);
+
+            while (reader.Peek() > -1)
+            {
+                Asistencia asistencia = new Asistencia();
+                asistencia.NoEmpleado = Convert.ToInt16(reader.ReadLine());
+                asistencia.HoraMes = Convert.ToInt16(reader.ReadLine());
+                asistencia.Mes = Convert.ToInt16(reader.ReadLine());
+
+                //guardar el empleado a la lista de empleados 
+                asistencias.Add(asistencia);
+            }
+            reader.Close();
+        }
+
+        public void MostrarEmpleados()
+        {
+
+            //mostrar la lista de empleados en el gridview 
+            dataGridViewEmpleados.DataSource = null;
+            dataGridViewEmpleados.DataSource = empleados;
+            dataGridViewEmpleados.Refresh();
+        }
+        public void MostrarAsistencia()
+        {
+
+            //mostrar la lista de empleados en el gridview 
+            dataGridViewAsistencia.DataSource = null;
+            dataGridViewAsistencia.DataSource = asistencias;
+            dataGridViewAsistencia.Refresh();
+        }
+
+        private void cargar_Click(object sender, EventArgs e)
+        {
+
+            CargarEmpleados();
+            MostrarEmpleados();
+            CargarAsistencia();
+            MostrarAsistencia();
+
+
+        }
+
+        private void calcular_Click(object sender, EventArgs e)
+        {
+            
+            foreach (Empleado empleado in empleados)
+            {
+                int noEmpleado = empleado.NoEmpleado;
+                foreach (Asistencia asistencia in asistencias)
+                {
+                    if (empleado.NoEmpleado == asistencia.NoEmpleado)
+                    {
+                        Reporte reporte = new Reporte();
+                        reporte.NombreEmpleado = empleado.Nombre;
+                        reporte.Mes = asistencia.Mes;
+                        reporte.SueldoMensual = empleado.SueldoHora * asistencia.HoraMes;
+
+                        reportes.Add(reporte);
+                    }
+                }
+            }
+            dataGridViewReporte.DataSource = null;
+            dataGridViewReporte.DataSource = reportes;
+            dataGridViewReporte.Refresh();
+        }
+    }
+    
+}
